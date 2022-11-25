b5f9abbe5e66154fc04a26903117f1a1743387ea
Sigmanificient <edhyjox@gmail.com>
2022-11-25 20:54:23 +0100

    :tada: Add frontend base

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,9 @@
 <script setup>
-// set message to request output of localhost:8080
-
-import { ref } from 'vue'
-
-const message = ref('')
-
-fetch('http://localhost:8080')
-    .then(response => response.json())
-    .then(text => message.value = text.text)
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <p><code>{{ message }}</code></p>
+    <TheWelcome />
   </main>
 </template>
