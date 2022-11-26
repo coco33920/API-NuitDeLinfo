4c39fc0243812ed3d1dbfd0286117a1c6cd20baf
Sigmanificient <edhyjox@gmail.com>
2022-11-26 17:19:48 +0100

    :sparkles: Hello, World!

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,17 @@
+<script setup>
+// set message to request output of localhost:8080
+
+import { ref } from 'vue'
+
+const message = ref('')
+
+fetch('http://localhost:8080')
+    .then(response => response.json())
+    .then(text => message.value = text.text)
+</script>
+
 <template>
   <main>
-    <p>Hello!</p>
+    <p><code>{{ message }}</code></p>
   </main>
 </template>
