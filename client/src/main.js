f3837c59ae58ebc047859928f9b58c7f24573088
sigmanificient <edhyjox@gmail.com>
2022-12-01 18:57:30 +0100

    :sparkles: Add Konami code setup

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from "vue";
+import KonamiCode from "vue3-konami-code";
+
 // import { createPinia } from "pinia";
 
 import App from "@/App.vue";
 import router from "@/router";
 
 const app = createApp(App);
+
+app.use(KonamiCode, {
+    onKonamiCodeEntered: function () {
+        alert("Konami Code successfully entered!");
+    },
+});
 
 // app.use(createPinia());
 app.use(router);
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,7 @@
-<script setup>
-// set message to request output of localhost:8080
-
-import { ref } from 'vue'
-
-const message = ref('')
-
-fetch('http://localhost:8080')
-    .then(response => response.json())
-    .then(text => message.value = text.text)
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <p><code>{{ message }}</code></p>
-  </main>
+    <main>
+        <p>Hello :D</p>
+    </main>
 </template>
