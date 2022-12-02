3a6247d095716fb8d09f847cad3b557802e50ca8
jaggernaute <lucas.videcoq1@gmail.com>
2022-12-02 01:38:42 +0100

    fix CORS problem

--- server/src/main/java/gay/bacoin/api/Server.java
+++ server/src/main/java/gay/bacoin/api/Server.java
@@ -270,6 +270,22 @@ public class Server {
     }
 
     private static void setupRoute() {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
         get("/", (request, response) -> {
             response.type("application/json");
             return "{\"text\":\"Hello World\"}";
